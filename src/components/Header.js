f25016efbf8cd45125b04210b5567a7b93594d6e
sgoal2 <sgoal27@gmail.com>
2019-01-16 15:13:35 -0500

    Added some pics

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,8 +9,10 @@ const Header = (props) => (
         <div className="content">
             <div className="inner">
                 <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <p>Hello, 
+I'm Sagie.
+
+I'm creating beautiful web applications and tools while continually learning in a tech focused world. </p>
             </div>
         </div>
         <nav>
